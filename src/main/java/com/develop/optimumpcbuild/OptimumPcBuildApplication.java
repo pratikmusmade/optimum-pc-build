58b817180a1de14e104fb949b677c526329d0c37
Pratik <pratikmusmadessjgod@gmail.com>
2023-12-04 11:17:08 +0530

    initial commit

---
+++ src/main/java/com/develop/optimumpcbuild/OptimumPcBuildApplication.java
@@ -0,0 +1,13 @@
+package com.develop.optimumpcbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimumPcBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimumPcBuildApplication.class, args);
+	}
+
+}
