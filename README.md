6271eaae61a19a59ebab449f38d9cf36aa58dc94
Pratik <pratikmusmadessjgod@gmail.com>
2023-12-12 19:15:55 +0530

    updated Readme for db config local

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # optimum-pc-build
+
+Steps for developers to setup database locally:
+(Until RDS instance is hosted)
+- Install PostgreSQL
+- Run the `/optimum-pc-build/sql/DB data optimum-pc-builds/optimum-pc-build-db.sql` file
+OR
+- Install psycopg2
+- Create tables manually using the `/optimum-pc-build/sql/DB data optimum-pc-builds/optimum-pc-build-db.sql` file
+- Load data using `/optimum-pc-build/sql/DB data optimum-pc-builds/insert_json_to_postgres.py` script. Initially scrapped data is stored in JSON files
--- sql/DB data optimum-pc-builds/insert_json_to_postgres.py
+++ sql/DB data optimum-pc-builds/insert_json_to_postgres.py
@@ -8,8 +8,11 @@ password = "Passw0rd!"
 host = "localhost"
 port = "5432"
 
+# Give table name as input for which data is to be loaded.
+table_name = ""
+
 # JSON file path
-json_file_path = "C:\\Users\\kishore sathe\\Desktop\\Workspace\\DB data optimum-pc-builds\\speaker.json"
+json_file_path = f"C:\\Users\\pratik musmade\\Desktop\\Workspace\\DB data optimum-pc-builds\\{table_name}.json"
 
 # Open and read the JSON file
 with open(json_file_path, encoding="utf8") as file:
@@ -23,16 +26,16 @@ connection = psycopg2.connect(
 # Create a cursor object to execute SQL queries
 cursor = connection.cursor()
 
-print(cursor.execute(f"select * from public.speaker"))
+print(cursor.execute(f"select * from public.{table_name}"))
 # Iterate through the data and insert it into the PostgreSQL table
 for row in data:
     columns = ", ".join(row.keys())
     values = ", ".join(["%s" for _ in row.values()])
-    insert_query = f"INSERT INTO public.speaker ({columns}) VALUES ({values});"
+    insert_query = f"INSERT INTO public.{table_name} ({columns}) VALUES ({values});"
     cursor.execute(insert_query, tuple(row.values()))
 
 # Commit the changes and close the connection/
-print(cursor.execute(f"select * from public.speaker"))
+print(cursor.execute(f"select * from public.{table_name}"))
 
 connection.commit()
 connection.close()
